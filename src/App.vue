1ea3222b3421f406c3c898c59aeffe075e8b5012
= <flaviohenriquealmeida@gmail.com>
2016-12-30 15:29:45 -0200

    menu component with router trick made by me

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="corpo">
+
+    <meu-menu :rotas="routes"/>
+    
     <transition name="pagina">
       <router-view></router-view>
     </transtion>
+
   </div>
 </template>
 <script>
+
+import { routes }  from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
+export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
+
+  data() {
+
+    return {
+
+      routes
+    }
+
+  }
+
+}
 </script>
 <style>
+  
   .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
@@ -15,10 +40,9 @@
   }
 
   .pagina-enter-active, .pagina-leave-active {
-    transition: opacity .2s
+    transition: opacity .3s
   }
   .pagina-enter, .pagina-leave-active {
     opacity: 0
   }
-
 </style>
