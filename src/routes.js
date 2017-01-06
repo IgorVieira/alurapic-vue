f9afc38c31822bc3fbfa374cbd37c73464b96d21
= <flaviohenriquealmeida@gmail.com>
2017-01-06 16:49:24 -0200

    c removed

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import Home from './components/home/Home.vue';
-// import Cadastro from './components/cadastro/Cadastro.vue';
-// const Cadastro = resolve => require(['./components/cadastro/Cadastro.vue'], resolve);
+
 const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
 
 
@@ -12,27 +11,3 @@ export const routes = [
     { path: '*', component: Home, menu: false }
 
 ];
-
-/*
-import Home from './components/home/Home.vue';
-// import Cadastro from './components/cadastro/Cadastro.vue';
-
-export const routes = [
-
-    { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
-    { 
-        path: '/cadastro', 
-        name:'cadastro', 
-        component: resolve => require(['./components/cadastro/Cadastro.vue'], resolve),
-        titulo: 'Cadastro', menu: true 
-    },
-    { 
-        path: '/cadastro/:id', 
-        name:'altera', 
-        component: resolve => require(['./components/cadastro/Cadastro.vue'], resolve), 
-        titulo: 'Cadastro', 
-        menu: false },
-    { path: '*', component: Home, menu: false }
-
-];
-*/
